e804fb50924b1b935e1f146e3a04186ef80efc88
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-11-01 19:43:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+
+url = "https://www.amazon.com.br/Celular-Xiaomi-Redmi-Note-12/dp/B0BZ7R2MPQ/"
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0",
+    "Accept-Language": "pt-BR",
+}
+
+resposta = requests.get(url, headers=headers)
+soup = BeautifulSoup(resposta.text, 'html.parser')  # Use 'html.parser' em vez de 'lxml'
+
+# Encontre o título do produto usando o seletor CSS apropriado
+titulo_do_produto = soup.select_one("#productTitle")
+
+if titulo_do_produto:
+    texto_do_titulo = titulo_do_produto.get_text(strip=True)
+    print(texto_do_titulo)
+else:
+    print("Título do produto não encontrado.")
