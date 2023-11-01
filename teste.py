32d5c86e230be28a73e4ddf6883b6b86f980c525
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-11-01 20:20:40 -0300

    v0

---
+++ teste.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+import smtplib
+import email.message
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+drive = webdriver.Firefox()
+
+url = "https://www.amazon.com.br/dp/B091G767YB/"
